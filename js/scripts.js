03514ac49c12a6e1dd7be37bd330260bc07b820d
KrAnna7 <anna.dzioch@gmail.com>
2017-04-08 16:55:28 +0200

    dodanie plików

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+// plik scripts.js
+
+var a = prompt('Podaj wartość podstawy trójkąta');
+	h = prompt('Podaj wartość wysokości trójkąta');
+	triangleArea = a*h/2;
+
+alert('bieżąca wartość pola trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi : ' + triangleArea);
+console.log( 'bieżąca wartość pola trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi : ' + triangleArea);
